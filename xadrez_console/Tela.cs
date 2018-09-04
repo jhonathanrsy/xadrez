0895ae72153c89a6e87154b61d51697fe12eb30a
unknown <jhonathan_hcl@hotmail.com>
2018-09-03 21:30:01 -0300

    implementando tela xadrez

--- xadrez_console/Tela.cs
+++ xadrez_console/Tela.cs
@@ -5,23 +5,25 @@ namespace xadrez_console
 {
     class Tela
     {
-        public static void imprimirTabuleiro( Tabuleiro tab)
+
+        public static void imprimirTabuleiro(Tabuleiro tab)
         {
-            for (int i=0; i<tab.linhas; i++)
+
+            for (int i = 0; i < tab.linhas; i++)
             {
-                for(int j=0; j<tab.colunas; j++)
+                for (int j = 0; j < tab.colunas; j++)
                 {
                     if (tab.peca(i, j) == null)
                     {
                         Console.Write("- ");
                     }
-                    
                     else
                     {
                         Console.Write(tab.peca(i, j) + " ");
-                    }  
+                    }
                 }
-            }  Console.WriteLine();   
+                Console.WriteLine();
+            }
         }
     }
 }
